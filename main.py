dc834649453ec6a88b50f3ce08460a678e63dba6
Shamil Asadullayev <asadullayevshamil94@gmail.com>
2026-08-26 11:39:38 +0400

    Just a cube

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for x in range(1, 31):
-    if x == 20:
-        continue
-    else:
-        print(x)
+rows = int(input("Enter the # of rows: "))
+colums = int(input("Enter the # of colums: "))
+symbol = input("Enter the symbol to use: ")
+
+for _ in range(rows):
+    for y in range(colums):
+        print(symbol, end="")
+    print()
