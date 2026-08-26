920e802b824b7fbc1090e9136b9ff52cc5fca1ba
Shamil Asadullayev <asadullayevshamil94@gmail.com>
2026-08-26 15:54:57 +0400

    Added Guess the Number game with attempts counter.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-rows = int(input("Enter the # of rows: "))
-colums = int(input("Enter the # of colums: "))
-symbol = input("Enter the symbol to use: ")
+import random
 
-for _ in range(rows):
-    for y in range(colums):
-        print(symbol, end="")
-    print()
+number = random.randint(1, 20)
+attempts = 0
+while True:
+    attempts += 1
+    guess = int(input("Guess my number(1,20): "))
+    # if the guess too high
+    if guess > number:
+        print("Too High take a lower")
+        # if guess too low
+    elif guess < number:
+        print("Too Low take a higher")
+        # when person guess it
+    if guess == number:
+        print(f"You guessed, my number is {number}")
+        # attempts
+        print(f"Your attempts are {attempts}")
+        break
