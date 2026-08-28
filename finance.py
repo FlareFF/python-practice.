48ca9443488011f85db8a41a3bf5a5063e28c6fa
Shamil Asadullayev <asadullayevshamil94@gmail.com>
2026-08-28 11:35:23 +0400

    feat: finalize compound interest

--- finance.py
+++ finance.py
@@ -1,11 +1,14 @@
 def calculate(principal, interest_rate, years):
-    amount = principal * pow((1 + interest_rate), years)
+    amount = principal
+
+    for year in range(1, years + 1):
+        amount *= 1 + interest_rate
     return amount
 
 
 principal = float(input("Enter the deposit: "))
 interest_rate = float(input("Enter the rate: "))
-years = float(input("Enter the years: "))
+years = int(input("Enter the years: "))
 
 if principal < 0 or interest_rate < 0 or years < 0:
     print("Error! All values must be greater than zero")
