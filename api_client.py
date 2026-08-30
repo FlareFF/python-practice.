efe73f56419e803d3535dd58ccea0bca219d48c7
Shamil Asadullayev <asadullayevshamil94@gmail.com>
2026-08-30 20:51:33 +0400

    git commit -m "Add weather API client to fetch current temperature"

--- api_client.py
+++ api_client.py
@@ -1,7 +1,19 @@
 import urllib.request
 import json
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=47.37&longitude=8.54&current_weather=true"
+city = input("Enter the city: ")
+geo_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+
+geo_response = urllib.request.urlopen(geo_url)
+
+data_geo_url = geo_response.read()
+
+geo_dict = json.loads(data_geo_url)
+
+latitude = geo_dict["results"][0]["latitude"]
+longitude = geo_dict["results"][0]["longitude"]
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
 
 response = urllib.request.urlopen(url)
 
